d1894d3c1ea515c5fcf6beec3c2e1d6a588d93ed
Seghir Nadir <nadir.seghir@gmail.com>
2020-07-16 10:55:23 +0100

    add algolia

--- src/App.js
+++ src/App.js
@@ -1,54 +1,73 @@
-import React, {useState, useEffect, useRef} from 'react';
-import 'normalize.css';
-import './App.scss';
-import elasticlunr from 'elasticlunr';
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, InfiniteHits } from 'react-instantsearch-dom';
+import React, { useState, useRef, useCallback } from "react";
+import "normalize.css";
+import "./App.scss";
+import algoliasearch from "algoliasearch/lite";
+import {
+  InstantSearch,
+  connectSearchBox,
+  connectStateResults,
+  InfiniteHits,
+} from "react-instantsearch-dom";
+import Episode from "./Episode";
 
-
-import shuffle from 'shuffle-array';
-import Episode from './Episode'
-import sourceEpisodes from './episodes.json';
-import searchIndex from './index.json';
-algoliasearch()
 const searchClient = algoliasearch(
-  'W963T74YEO',
-  '75f3fd4f3d8092f7548023ea7c3077d0'
+  "W963T74YEO",
+  "75f3fd4f3d8092f7548023ea7c3077d0"
 );
+const SearchBox = ({ refine, delay, currentRefinement, ...props }) => {
+  const timerId = useRef(null);
+  const [value, setValue] = useState(currentRefinement);
 
+  const onChangeDebounced = useCallback(
+    (event) => {
+      const _value = event.currentTarget.value;
+
+      clearTimeout(timerId.current);
+      timerId.current = setTimeout(() => refine(_value), delay);
+
+      setValue(_value);
+    },
+    [delay, refine]
+  );
+
+  return (
+    <input
+      value={value}
+      type="search"
+      onChange={onChangeDebounced}
+      {...props}
+    />
+  );
+};
+
+const Results = connectStateResults((props) => {
+  console.log(props);
+  const { searchState, searchResults, isSearchStalled, children } = props;
+  return isSearchStalled || (searchResults && searchResults.nbHits !== 0) ? (
+    children
+  ) : (
+    <div>No results have been found for {searchState.query}.</div>
+  );
+});
+const DebouncedSearchBox = connectSearchBox(SearchBox);
 function App() {
-  const [search, setSearch] = useState("");
-  const [episodes, setEpisodes] = useState([]);
-  const idx = useRef();
-  useEffect(() => {
-    idx.current = elasticlunr.Index.load(searchIndex);
-
-  })
-  useEffect(() => {
-    if (!search) {
-      setEpisodes(shuffle.pick(sourceEpisodes, {picks: 10, copy: true}));
-    } else {
-      const results = idx.current.search(search, {
-        fields: {
-            title: {boost: 10,bool: "AND"},
-            plot: {boost: 1}
-        },
-        bool: "AND"
-    });
-      setEpisodes(sourceEpisodes.filter(({link}) => results.find(({ref}) => ref === link)))
-    }
-  }, [search])
   return (
     <div className="App">
       <h1 className="page-title">The One With All The Episodes</h1>
-      <InstantSearch className="search-box"
-    indexName="friends"
-
-    searchClient={searchClient}
-  >
-    <SearchBox className='search-input' placeholder="Search for an episode or a number" autoFocus={true} submit={null}  />
-    <InfiniteHits hitComponent={Episode} />
-  </InstantSearch>
+      <InstantSearch
+        className="search-box"
+        indexName="friends"
+        searchClient={searchClient}
+      >
+        <DebouncedSearchBox
+          delay={1000}
+          className="search-input"
+          placeholder="Search for an episode or a number"
+        />
+        <Results>
+          <InfiniteHits hitComponent={Episode} />
+        </Results>
+      </InstantSearch>
     </div>
   );
 }
--- src/Episode.js
+++ src/Episode.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import './Episode.scss';
-import ClampText from 'react-clamp-text';
-import { Highlight } from 'react-instantsearch-dom';
-export default function Episode({hit}) {
-	const {  image, number, link, season, _highlightResult} = hit;
-	const {title: {value: title}, plot: {value: plot}} = _highlightResult;
-	return (
-		<a href={link} target="_blank" rel="noopener noreferrer" className="episode">
-			<img src={`/img/${image}`} alt={title} width='33%' height='auto' />
-			<div className="episode-content">
-				<h3><Highlight attribute="title" hit={hit} /></h3>
-				<ClampText className="plot" line={3} showMore={false}><Highlight attribute="plot" hit={hit} /></ClampText>
-			</div>
-		</a>
-	)
+import React from "react";
+import "./Episode.scss";
+import ClampText from "react-clamp-text";
+import { Highlight } from "react-instantsearch-dom";
+export default function Episode({ hit }) {
+  const { image, number, link, season, _highlightResult } = hit;
+  const {
+    title: { value: title },
+  } = _highlightResult;
+  const EpisodeNumber = (props) =>
+    `S${props.season.toLocaleString("en", {
+      minimumIntegerDigits: 2,
+    })}E${props.number.toLocaleString("en", { minimumIntegerDigits: 2 })}: `;
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="episode"
+    >
+      <img src={`/img/${image}`} alt={title} width="33%" height="auto" />
+      <div className="episode-content">
+        <h3>
+          <EpisodeNumber season={season} number={number} />
+          <Highlight attribute="title" hit={hit} />
+        </h3>
+        <ClampText className="plot" line={3} showMore={false}>
+          <Highlight attribute="plot" hit={hit} />
+        </ClampText>
+      </div>
+    </a>
+  );
 }
